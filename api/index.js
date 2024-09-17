6bd3387d6306551d1ca20be2f55835cc0048907d
Gerome Fischer <geromefischer@gnx.de>
2024-09-17 16:39:02 +0200

    Added backend in vercel and environment setup

--- api/index.js
+++ api/index.js
@@ -1,3 +1,5 @@
+// api/index.js
+
 const express = require('express');
 const serverless = require('serverless-http');
 const dotenv = require('dotenv');
@@ -9,7 +11,7 @@ dotenv.config();
 // Initialize Firebase Admin SDK using the service account
 const serviceAccount = {
   "type": "service_account",
-  "project_id": "theideahunt-74cd8",
+  "project_id": process.env.FIREBASE_PROJECT_ID,
   "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
   "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
   "client_email": process.env.FIREBASE_CLIENT_EMAIL,
